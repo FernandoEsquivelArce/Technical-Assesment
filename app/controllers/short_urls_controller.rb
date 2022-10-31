dbc2696502cfc5a8458494057bbbec27fcce8e2c
Fernando2000 <fernando.esquivel.arce@gmail.com>
2022-10-31 00:36:22 -0600

    Validate URL

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,9 +4,18 @@ class ShortUrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    @urls=ShortUrl.all 
   end
 
   def create
+    
+    @url = ShortUrl.new(full_url:params[:full_url])
+    print(@url.valid?)
+    if @url.save 
+      render json: @url,status:200
+    else
+      render json: {error:@url.errors}
+    end
   end
 
   def show
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,8 +1,8 @@
 class ShortUrl < ApplicationRecord
-
+  require 'uri'
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
-
   validate :validate_full_url
+
 
   def short_code
   end
@@ -10,9 +10,13 @@ class ShortUrl < ApplicationRecord
   def update_title!
   end
 
+
   private
 
   def validate_full_url
+    uri = URI.parse(full_url)
+    uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  rescue URI::InvalidURIError
+    errors.add(:full_url,'Invalid_url')
   end
-
 end
